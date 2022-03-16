b7ecf47fb9e37c38dbc73991076fc7a7fea71c1e
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2022-03-16 16:57:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """FastAPI serving HTTP healthcheck endpoint"""
 
 import os
-import asyncio
+# import asyncio
 
-import discord
-import requests
+# import discord
+# import requests
 
 from fastapi import FastAPI
 from azure.keyvault.secrets import SecretClient
@@ -19,39 +19,39 @@ vault_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 cat_apikey = vault_client.get_secret('cat-apikey')
 bot_token = vault_client.get_secret('discord-bot-token')
 
-headers = {
-    'x-api-key': cat_apikey
-}
+# headers = {
+#     'x-api-key': cat_apikey
+# }
 
 
-class DiscordClient(discord.Client):
+# class DiscordClient(discord.Client):
 
-    async def on_ready(self):
-        print('Logged on as', self.user)
+#     async def on_ready(self):
+#         print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+#     async def on_message(self, message):
+#         # don't respond to ourselves
+#         if message.author == self.user:
+#             return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+#         if message.content == 'ping':
+#             await message.channel.send('pong')
 
-        if message.content == 'cat':
-            resp = requests.get('https://api.thecatapi.com/v1/images/search', headers=headers).json()
-            await message.channel.send(resp[0])
+#         if message.content == 'cat':
+#             resp = requests.get('https://api.thecatapi.com/v1/images/search', headers=headers).json()
+#             await message.channel.send(resp[0])
 
 
 api = FastAPI(docs_url=None)
 
-client = DiscordClient()
+# client = DiscordClient()
 
 
-@api.on_event('startup')
-async def startup_event():
-    asyncio.create_task(client.start(bot_token))
+# @api.on_event('startup')
+# async def startup_event():
+#     asyncio.create_task(client.start(bot_token))
 
 
 @api.get('/')
 async def health_check():
-    return {'status': 'OK'}
+    return {'status': 'OK', 'apikey': cat_apikey, 'botkey': bot_token}
