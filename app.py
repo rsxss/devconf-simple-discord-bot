0bd98d808cf4d379f419c8814e8f006abe53eac4
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2022-03-16 13:46:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 import os
 import asyncio
-import threading
 
 import discord
 import requests
@@ -20,42 +19,39 @@ client = SecretClient(vault_url=key_vault_uri, credential=credential)
 cat_apikey = client.get_secret('cat-apikey')
 bot_token = client.get_secret('discord-bot-token')
 
+headers = {
+    'x-api-key': cat_apikey
+}
+
+
+class DiscordClient(discord.Client):
+
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+        if message.content == 'cat':
+            resp = requests.get('https://api.thecatapi.com/v1/images/search', headers=headers).json()
+            await message.channel.send(resp[0])
+
+
 api = FastAPI(docs_url=None)
+
+bot_client = DiscordClient()
+
+
+@api.on_event('startup')
+async def startup_event():
+    asyncio.create_task(bot_client.start(bot_token))
 
 
 @api.get('/')
 async def health_check():
     return {'status': 'OK'}
-
-
-def bot_thread(token):
-    _loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(_loop)
-
-    headers = {
-        'x-api-key': cat_apikey
-    }
-
-    class DiscordClient(discord.Client):
-
-        async def on_ready(self):
-            print('Logged on as', self.user)
-
-        async def on_message(self, message):
-            # don't respond to ourselves
-            if message.author == self.user:
-                return
-
-            if message.content == 'ping':
-                await message.channel.send('pong')
-
-            if message.content == 'cat':
-                resp = requests.get('https://api.thecatapi.com/v1/images/search', headers=headers).json()
-                await message.channel.send(resp[0])
-
-    bot_client = DiscordClient()
-    bot_client.run(token)
-
-
-bot = threading.Thread(target=bot_thread, args=(bot_token,))
-bot.start()
